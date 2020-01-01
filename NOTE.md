3a422c349ed1ec22206b19d2ba1229e8c4436d6b
hellomac87 <hellomac87@gmail.com>
2020-01-01 22:04:08 +0900

    find, includes

--- NOTE.md
+++ NOTE.md
@@ -97,3 +97,20 @@ console.log(isBiggerThenOne);
 ```
 
 ### 13. find, includes
+
+```js
+'use strict';
+
+// find, includes
+
+const arr = ['node.js','올인원'];
+
+// 해당 조건의 데이터 리턴
+const ret = arr.find(key => key === '올인원');
+
+// 해당 데이터가 포함되어있는지 여부
+const res = arr.includes('node.js');
+
+console.log(ret);
+console.log(res);
+```
--- test/fast.js
+++ test/fast.js
@@ -1,7 +1,14 @@
 'use strict';
 
-const arr = [2,3,4,5,6];
+// find, includes
 
-const isBiggerThenOne = arr.every(key => key > 2);
+const arr = ['node.js','올인원'];
 
-console.log(isBiggerThenOne);
+// 해당 조건의 데이터 리턴
+const ret = arr.find(key => key === '올인원');
+
+// 해당 데이터가 포함되어있는지 여부
+const res = arr.includes('node.js');
+
+console.log(ret);
+console.log(res);
