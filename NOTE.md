5724123e5c690a815d8b8231e78178f8a7980e45
hellomac87 <hellomac87@gmail.com>
2020-01-02 21:02:13 +0900

    filter, map

--- NOTE.md
+++ NOTE.md
@@ -132,3 +132,27 @@ arr.forEach((item) => {
 console.log(newArr);
 // [1,2,3]
 ```
+
+### 15. map, filter
+
+```js
+'use strict';
+
+const arr = ['node.js','올인원'];
+
+// filter
+
+const newArr = arr.filter(data => data==='node.js');
+
+console.log(newArr);
+
+// map
+// 기존의 배열이나 객체에서 새로운 데이터를 가공해야 하는 경우
+
+const mapArr = arr.map(item => {
+    title: item
+})
+
+console.log(mapArr);
+
+```
--- test/fast.js
+++ test/fast.js
@@ -1,12 +1,18 @@
 'use strict';
 
-// forEach
+const arr = ['node.js','올인원'];
 
-const arr = [1,2,3];
-const newArr = [];
-arr.forEach((item) => {
-    newArr.push(item);
-});
+// filter
+
+const newArr = arr.filter(data => data==='node.js');
 
 console.log(newArr);
-// [1,2,3]
+
+// map
+// 기존의 배열이나 객체에서 새로운 데이터를 가공해야 하는 경우
+
+const mapArr = arr.map(item => {
+    title: item
+})
+
+console.log(mapArr);
