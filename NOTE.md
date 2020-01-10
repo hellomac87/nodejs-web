f11120a35e5efa7ca9cd2c3689415d2c78c0a49f
hellomac87 <hellomac87@gmail.com>
2020-01-10 21:26:47 +0900

    hoisting

--- NOTE.md
+++ NOTE.md
@@ -327,3 +327,24 @@ const number = 1;
 console.log(typeof number);
 
 ```
+
+### 27. hoisting
+```js
+'use strict';
+
+// hoisting
+console.log(var1);
+
+// let var1 = 'r';
+var var1 = 'r';
+
+
+fn();
+
+function fn(){
+    console.log('fn');
+}
+
+// var 는 미리 선언하지 않아도 전역변수로 선언되기 때문에
+// 나중에 선언되더라도 코드의 최상단에 선언된것 처럼 실행한다.
+```
--- test/fast.js
+++ test/fast.js
@@ -1,8 +1,17 @@
 'use strict';
 
-const string ='node.js';
-const array = [];
-const obj = {};
-const number = 1;
+// hoisting
+console.log(var1);
 
-console.log(typeof number);
+// let var1 = 'r';
+var var1 = 'r';
+
+
+fn();
+
+function fn(){
+    console.log('fn');
+}
+
+// var 는 미리 선언하지 않아도 전역변수로 선언되기 때문에
+// 나중에 선언되더라도 코드의 최상단에 선언된것 처럼 실행한다.
