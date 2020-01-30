f118c05a6f6ec9cdd274c81787b6a1dc0c341ad3
hellomac87 <hellomac87@gmail.com>
2020-01-30 22:02:07 +0900

    state hook

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,14 +1,58 @@
 import React, {useState} from 'react'
+// react 는 재사용 가능한 component 기반의 UI liberary다
 
-const App = () => {
-  const [count, setCount] = useState(0)
+const Todo = ({todo, index, completeTodo, removeTodo}) => {
+  return(
+  <div key={index}>{todo}</div>
+  )
+}
+
+const TodoForm = ({addTodo}) => {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!value) return;
+
+    addTodo(value);
+    
+    setValue('');
+  };
 
   return(
-    <div>
-      {count}
-      <button onClick={() => setCount(count + 1)}>+</button>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  )
+}
+
+const App = () => {
+  const [todo, setTodo] = useState([]);
+
+  return(
+    <div className="app">
+      <div className="todo-list">
+        {
+          todo.map((item, index) => (
+            <Todo
+              key={item}
+              index={index}
+              todo={todo}
+              completeTodo={completeTodo}
+              removeTodo={removeTodo}
+            />
+          ))
+        }
+        <TodoForm addTodo={addTodo}/>
+      </div>
     </div>
   )
+  
 }
 
 export default App;
