05eef75b27815dfb887dfddaa802de4738d80b7a
hellomac87 <hellomac87@gmail.com>
2020-02-08 09:45:00 +0900

    Race Conditions

--- NOTE.md
+++ NOTE.md
@@ -1158,3 +1158,65 @@ for(const item of arr){
   // 비동기 코드 가능
 }
 ```
+
+```js
+// ex) 은행 이체, transaction,
+// race condition 에 의해서 비정상작동 하는 경우가 있어서는 안됨
+
+let total = 0;
+
+async function getTotal() {
+  return total;
+}
+
+async function setTotal(value) {
+  return value;
+}
+
+async function increament(value, inc) {
+  return value + inc;
+}
+
+async function add() {
+  let current, newValue;
+  current = await getTotal();
+  newValue = await increament(current, 20);
+  await setTotal(newValue);
+}
+
+async function main() {
+  let transaction1, transaction2;
+
+  // 두개의 조건이 경쟁상태에 있음
+  transaction1 = add();
+  transaction2 = add();
+
+  await transaction1;
+  await transaction2;
+
+  console.log(await getTotal());
+}
+
+main();
+
+// class 로 해결
+// 경쟁상태를 만들지 않겠다.
+class Lock {
+  constructor() {
+    this._locked = false;
+    // queu, 대기할 수 있는 자료구조
+    this._waiting = [];
+  }
+
+  lock() {
+    // 잠그는 과정과
+    // 푸는 과정이 존재
+    const unlock = () => {
+      let nextResolve;
+      if (this._waiting.length) {
+        // 강의 화면이 멈춤...씨뻘놈들진짜...
+      }
+    };
+  }
+}
+```
--- test/fast.js
+++ test/fast.js
@@ -1,20 +1,59 @@
-"use strict";
+// ex) 은행 이체, transaction,
+// race condition 에 의해서 비정상작동 하는 경우가 있어서는 안됨
 
-const arr = [some Promise, some Promise, some Promise];
+let total = 0;
 
-arr.map(item => {
-  // 비동기 코드, 비동기적으로 실행
-  // 어떤 Promise 가 먼저 실행되는지 알 수 없고
-  // map function 안에서 async await 를 사용하면 안된다.
-  // item간 실행 완료 순서를 보장하지 않는다.
-});
-
-arr.forEach(item => {
-  // 비동기코드 불가능(작동은 하나 순서를 보장하지 않음)
-})
-
-for(const item of arr){
-  // 비동기 코드 가능
+async function getTotal() {
+  return total;
 }
 
+async function setTotal(value) {
+  return value;
+}
 
+async function increament(value, inc) {
+  return value + inc;
+}
+
+async function add() {
+  let current, newValue;
+  current = await getTotal();
+  newValue = await increament(current, 20);
+  await setTotal(newValue);
+}
+
+async function main() {
+  let transaction1, transaction2;
+
+  // 두개의 조건이 경쟁상태에 있음
+  transaction1 = add();
+  transaction2 = add();
+
+  await transaction1;
+  await transaction2;
+
+  console.log(await getTotal());
+}
+
+main();
+
+// class 로 해결
+// 경쟁상태를 만들지 않겠다.
+class Lock {
+  constructor() {
+    this._locked = false;
+    // queu, 대기할 수 있는 자료구조
+    this._waiting = [];
+  }
+
+  lock() {
+    // 잠그는 과정과
+    // 푸는 과정이 존재
+    const unlock = () => {
+      let nextResolve;
+      if (this._waiting.length) {
+        // 강의 화면이 멈춤...씨뻘놈들진짜...
+      }
+    };
+  }
+}
